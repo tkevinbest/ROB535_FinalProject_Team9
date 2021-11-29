d5431e15e513bbf2aec35422fd50e3712eb201c3
wamiogunbi <wogunbi@gmail.com>
2021-11-29 17:12:34 -0500

    zigging and zagging yaaass

--- Control/avoidObstacles.m
+++ Control/avoidObstacles.m
@@ -1,4 +1,4 @@
-close all 
+close all
 clear
 
 %% Load/plot track and obstacles
@@ -18,7 +18,7 @@ figure(1)
 hold on
 plot(left_track(1,:), left_track(2,:),'k');
 plot(right_track(1,:), right_track(2,:),'k');
-plot(center_line(1,:), center_line(2,:),'--k');
+scatter(center_line(1,:), center_line(2,:));
 
 for i = 1:Nobs
     obj = line([Xobs{i}(:,1)', Xobs{i}(1,1)],[Xobs{i}(:,2)',Xobs{i}(1,2)]);
@@ -34,15 +34,19 @@ prev_point = center_line(:,1); % tracks previous point on center line
 obj_dectected = false; % tracks if object is dectected
 obj_location = zeros(size(Xobs{1})); % will store location of object that is detected
 
-tolerance = 0.75; % tolerance to determine how close object needs to be on trajectory to trigger evasive maneuvers 
+tolerance = 0.8; % tolerance to determine how close object needs to be on trajectory to trigger evasive maneuvers
+tolerance_obj_dist = 15; % tolerance to determine how close object needs to be on trajectory to trigger evasive maneuvers
 
 num_points = 10; % number of points to create between two points in trajectory
 traj_idx = 2; % stores where we are in trajectory
 traj_idx_increment = 1; % stores how much to increase trajectory counter by
 
+j = 1; %objects are presented in order
+avoid_step = 0;
+
 % plan route
 while traj_idx < length(center_line)
-    curr_point = center_line(:,traj_idx); 
+    curr_point = center_line(:,traj_idx);
     
     % %%%%%%% Detect Collisions %%%%%%%
     % interpolate line between current point and previous
@@ -55,66 +59,59 @@ while traj_idx < length(center_line)
         intercept = y_vector(i) - slope * x_vector(i);
         
         % dectect if current location collides with any object on trajectory
-        for j = 1:Nobs
+        if j <= size(Xobs,2)
             for k = 1:4 % check all 4 corners of object for impact with center line
-                if abs(abs(Xobs{j}(k,2)) - abs(slope*Xobs{j}(k,1) + intercept)) <= tolerance
+                distance_to_obj = sqrt((y_vector(i) - Xobs{j}(k,2))^2 + (x_vector(i) - Xobs{j}(k,1))^2);%add in a distance to next object being less than the 10 steps
+                obj_line = abs(abs(Xobs{j}(k,2)) - abs(slope*Xobs{j}(k,1) + intercept));
+                if distance_to_obj < tolerance_obj_dist &&  obj_line <= tolerance
                     obj_dectected = true;
                     obj_location = Xobs{j};
+                    continue
                 end
+                
             end
         end
     end
     
-    % if object is detected plan evasive maneuvers 
-    if obj_dectected 
+    % if object is detected plan evasive maneuvers
+    if obj_dectected
+        disp(['Object ' num2str(j) ' Detected'])
         % %%%%%%% Find min distance between closest points on right/left that are closest to object and object %%%%%%%
         % find closest points on left and right side of track respectively to detected object
-        idx_right = knnsearch(right_track(:,traj_idx:traj_idx+3)', obj_location);
-        idx_left = knnsearch(left_track(:,traj_idx-1:traj_idx+3)', obj_location);
-        
-        point_right1 = right_track(:,traj_idx + mode(idx_right) - 1);
-        point_right2 = right_track(:,traj_idx + mode(idx_right));
-        point_left1 = left_track(:,traj_idx + mode(idx_left) - 1);
-        point_left2 = left_track(:,traj_idx + mode(idx_left));
-        
-        % determine line between consecutive points on left and right line
-        slope_right = (point_right1(2) - point_right2(2))/(point_right1(1) - point_right2(1));
-        intercept_right = point_right1(2) - slope_right * (point_right2(1));
-        
-        slope_left = (point_left1(2) - point_left2(2))/(point_left1(1) - point_left2(1));
-        intercept_left = point_left1(2) - slope_left * (point_left1(1));
-        
-        % find closest intercept on object for right and left lines
-        min_dist_right = inf;
-        min_dist_left = inf;
-        for k = 1:4 % check all 4 corners of object for impact with center line
-            if abs(abs(obj_location(k,2)) - abs(slope_right*obj_location(k,1) + intercept_right)) < min_dist_right
-                min_dist_right = abs(abs(obj_location(k,2)) - abs(slope_right*obj_location(k,1) + intercept_right));
-            end
-            
-            if abs(abs(obj_location(k,2)) - abs(slope_left*obj_location(k,1) + intercept_left)) < min_dist_left
-                min_dist_left = abs(abs(obj_location(k,2)) - abs(slope_left*obj_location(k,1) + intercept_left));
-            end
-        end
-        
+                obs_center = center_obs(obj_location);
+
+        [idx_right, dist_right] = knnsearch(right_track(:,traj_idx)', obs_center);
+        [idx_left, dist_left] = knnsearch(left_track(:,traj_idx)', obs_center);
+
         % %%%%%%% Avoid object %%%%%%%
         % assign whether object is located on left or right side, determined by which minimum distance is smallest
-        if min_dist_right < min_dist_left % if object is on right
-            route(:,traj_idx) = (left_track(:,traj_idx)+center_line(:,traj_idx)) ./ 2;
+        if dist_right < dist_left % if object is on right
+            route(:,traj_idx:traj_idx+1) = (left_track(:,traj_idx:traj_idx+1)+center_line(:,traj_idx:traj_idx+1)) ./ 2;
+            disp(['Zagged RIGHT to avoid Object ' num2str(j)])
+            
         else % if object is on left
-            route(:,traj_idx) = (right_track(:,traj_idx)+center_line(:,traj_idx)) ./ 2; 
+            route(:,traj_idx:traj_idx+1) = (right_track(:,traj_idx:traj_idx+1)+center_line(:,traj_idx:traj_idx+1)) ./ 2;
+            disp(['Zigged LEFT to avoid Object ' num2str(j)])
         end
-
+        
+        j = j+1; %onto next object!
     end
     
     % %%%%%%% reset/update values %%%%%%%
+    plot(route(1,1:traj_idx),route(2,1:traj_idx),'b')
     obj_dectected = false;
     obj_location = zeros(size(Xobs{1}));
-
+    
     prev_point = curr_point;
     traj_idx = traj_idx + traj_idx_increment;
     
+    
 end
-
 % plot final route
 plot(route(1,:),route(2,:),'b')
+
+function point = center_obs(obs)
+    point(1) = mean(obs(:,1));
+    point(2) = mean(obs(:,2));
+end
+
