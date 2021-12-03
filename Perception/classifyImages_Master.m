fc6cb06147dcbc557d740d7f96e428a2c1ad2393
Adam Goertz <adambgoertz@gmail.com>
2021-12-02 21:22:44 -0500

    WIP: Try FasterRCNN example

--- Perception/classifyImages_Master.m
+++ Perception/classifyImages_Master.m
@@ -12,6 +12,41 @@ trainLabels = readtable((fullfile(imageFolder_Training, 'labels.csv')));
 trainingImages = imageDatastore(imageFolder_Training,'IncludeSubfolders',true, 'Labels',categorical(trainLabels.label)); 
 testingImages = imageDatastore(imageFolder_Testing, 'IncludeSubfolders',true); 
 
+bboxFiles = strrep(trainingImages.Files, '_image.jpg', '_bbox.bin');
+projMatFiles = strrep(trainingImages.Files, '_image.jpg', '_proj.bin');
+
+bboxes = cell(numel(bboxFiles), 1);
+for i = 1:numel(bboxFiles)
+    % Get centroid and size of bounding box. This is in 3d coordinates. 
+    % We need 2d pixel coordinates for bounding box regression (maybe).
+    bbox = reshape(read_bin(bboxFiles{i}), 11, []);
+    proj = reshape(read_bin(projMatFiles{i}), 4, 3);
+    
+    assert(numel(bbox) == 11);
+    
+    R = rot(bbox(1:3));
+    t = reshape(bbox(4:6), [3, 1]);
+    sz = bbox(7:9);
+    [vert_3D, edges] = get_bbox(-sz / 2, sz / 2);
+    vert_3D = R * vert_3D + t;
+    vert_2D = proj' * [vert_3D; ones(1, size(vert_3D, 2))];
+    vert_2D = vert_2D ./ vert_2D(3, :);
+    
+    x = round(min(vert_2D(1, :)));
+    y = round(min(vert_2D(2, :)));
+    w = round(max(vert_2D(1, :)) - x);
+    h = round(max(vert_2D(2, :)) - y);
+    
+    bboxes{i, 1} = [x y w h];
+end
+
+bboxTable = table(bboxes);
+
+trainingBboxes = boxLabelDatastore(bboxTable);
+
+trainingCombined = combine(trainingImages, trainingBboxes);
+test = read(trainingCombined);
+
 %% Examples to make sure things worked
 % Make a subplot of the first 3 types of images
 showImages = false; 
@@ -36,17 +71,135 @@ minExamples = median(imageCounts.Count);
 % trainingImages = splitEachLabel(trainingImages, minExamples, 'randomize'); 
 
 %% Load pretrained network for feature extraction
-% featureNetwork = resnet50(); 
-featureNetwork = googlenet(); 
-% featureNetwork = resnet101();
+featureNetwork = resnet50(); 
 
-desiredImageSize = featureNetwork.Layers(1).InputSize; 
+lgraph = layerGraph(featureNetwork);
+
+% Remove the last 3 layers. 
+layersToRemove = {
+    'fc1000'
+    'fc1000_softmax'
+    'ClassificationLayer_fc1000'
+    };
+lgraph = removeLayers(lgraph, layersToRemove);
+
+% Specify the number of classes the network should classify.
+numClasses = 3;
+numClassesPlusBackground = numClasses + 1;
+
+% Define new classification layers.
+newLayers = [
+    fullyConnectedLayer(numClassesPlusBackground, 'Name', 'rcnnFC')
+    softmaxLayer('Name', 'rcnnSoftmax')
+    classificationLayer('Name', 'rcnnClassification')
+    ];
+
+% Add new object classification layers.
+lgraph = addLayers(lgraph, newLayers);
+
+% Connect the new layers to the network. 
+lgraph = connectLayers(lgraph, 'avg_pool', 'rcnnFC');
+
+% Define the number of outputs of the fully connected layer.
+numOutputs = 4 * numClasses;
+
+% Create the box regression layers.
+boxRegressionLayers = [
+    fullyConnectedLayer(numOutputs,'Name','rcnnBoxFC')
+    rcnnBoxRegressionLayer('Name','rcnnBoxDeltas')
+    ];
+
+% Add the layers to the network.
+lgraph = addLayers(lgraph, boxRegressionLayers);
+
+% Connect the regression layers to the layer named 'avg_pool'.
+lgraph = connectLayers(lgraph,'avg_pool','rcnnBoxFC');
+
+% Select a feature extraction layer.
+featureExtractionLayer = 'activation_40_relu';
+
+% Disconnect the layers attached to the selected feature extraction layer.
+lgraph = disconnectLayers(lgraph, featureExtractionLayer,'res5a_branch2a');
+lgraph = disconnectLayers(lgraph, featureExtractionLayer,'res5a_branch1');
+
+% Add ROI max pooling layer.
+outputSize = [14 14];
+roiPool = roiMaxPooling2dLayer(outputSize,'Name','roiPool');
+lgraph = addLayers(lgraph, roiPool);
+
+% Connect feature extraction layer to ROI max pooling layer.
+lgraph = connectLayers(lgraph, featureExtractionLayer,'roiPool/in');
+
+% Connect the output of ROI max pool to the disconnected layers from above.
+lgraph = connectLayers(lgraph, 'roiPool','res5a_branch2a');
+lgraph = connectLayers(lgraph, 'roiPool','res5a_branch1');
+
+% Define anchor boxes. TODO: Compute better anchor box sizes
+% use estimateAnchorBoxes(boxLabelDatastore, numAnchors)
+anchorBoxes = [
+    16 16
+    32 16
+    16 32
+    ];
+
+% Create the region proposal layer.
+proposalLayer = regionProposalLayer(anchorBoxes,'Name','regionProposal');
+
+lgraph = addLayers(lgraph, proposalLayer);
+
+% Number of anchor boxes.
+numAnchors = size(anchorBoxes,1);
+
+% Number of feature maps in coming out of the feature extraction layer. 
+numFilters = 1024;
+
+rpnLayers = [
+    convolution2dLayer(3, numFilters,'padding',[1 1],'Name','rpnConv3x3')
+    reluLayer('Name','rpnRelu')
+    ];
+
+lgraph = addLayers(lgraph, rpnLayers);
+
+% Connect to RPN to feature extraction layer.
+lgraph = connectLayers(lgraph, featureExtractionLayer, 'rpnConv3x3');
+
+% Add RPN classification layers.
+rpnClsLayers = [
+    convolution2dLayer(1, numAnchors*2,'Name', 'rpnConv1x1ClsScores')
+    rpnSoftmaxLayer('Name', 'rpnSoftmax')
+    rpnClassificationLayer('Name','rpnClassification')
+    ];
+lgraph = addLayers(lgraph, rpnClsLayers);
+
+% Connect the classification layers to the RPN network.
+lgraph = connectLayers(lgraph, 'rpnRelu', 'rpnConv1x1ClsScores');
+
+% Add RPN regression layers.
+rpnRegLayers = [
+    convolution2dLayer(1, numAnchors*4, 'Name', 'rpnConv1x1BoxDeltas')
+    rcnnBoxRegressionLayer('Name', 'rpnBoxDeltas');
+    ];
+
+lgraph = addLayers(lgraph, rpnRegLayers);
+
+% Connect the regression layers to the RPN network.
+lgraph = connectLayers(lgraph, 'rpnRelu', 'rpnConv1x1BoxDeltas');
+
+% Connect region proposal network.
+lgraph = connectLayers(lgraph, 'rpnConv1x1ClsScores', 'regionProposal/scores');
+lgraph = connectLayers(lgraph, 'rpnConv1x1BoxDeltas', 'regionProposal/boxDeltas');
+
+% Connect region proposal layer to roi pooling.
+lgraph = connectLayers(lgraph, 'regionProposal', 'roiPool/roi');
+
+analyzeNetwork(lgraph);
+
+desiredImageSize = lgraph.Layers(1).InputSize; 
 resizedTrainingSet = augmentedImageDatastore(desiredImageSize, trainingImages, 'ColorPreprocessing', 'gray2rgb'); 
 
 % Skip the softmax and final classification layer
-% featureLayer = 'fc1000';
-featureLayer = 'loss3-classifier';
-activationsAtFeatureLayer = activations(featureNetwork, resizedTrainingSet, featureLayer, 'OutputAs', 'columns','ExecutionEnvironment','gpu');
+featureLayer = 'fc1000';
+activationsAtFeatureLayer = activations(lgraph, resizedTrainingSet, featureLayer, 'OutputAs', 'columns');
 numFeaturesFound = size(activationsAtFeatureLayer,1); 
 
 %% Train a classifier from the extracted features using SVM
@@ -76,10 +229,10 @@ options = trainingOptions('adam', ...
     'MiniBatchSize',500, ...
     'Plots','training-progress');
 
-[classifier, trainingInfo] = trainNetwork(activationsAtFeatureLayer', trainingImages.Labels,classifierLayers ,options);
+[classifier, trainingInfo] = trainFasterRCNNOBjectDetector(trainingImages, lgraph ,options);
 
 %% Evaluate
 resizeTestingSet = augmentedImageDatastore(desiredImageSize, testingImages,'ColorPreprocessing','gray2rgb'); 
-testFeatures = activations(featureNetwork, resizeTestingSet, featureLayer, 'OutputAs','columns','ExecutionEnvironment','gpu'); 
+testFeatures = activations(lgraph, resizeTestingSet, featureLayer, 'OutputAs','columns'); 
 testLabels = classify(classifier, testFeatures');
 writeOutputFile(testingImages.Files, testLabels); 
---
+++ Perception/rot.m
@@ -0,0 +1,10 @@
+function R = rot(n)
+theta = norm(n, 2);
+if theta
+    n = n / theta;
+    K = [0, -n(3), n(2); n(3), 0, -n(1); -n(2), n(1), 0];
+    R = eye(3) + sin(theta) * K + (1 - cos(theta)) * K^2;
+else
+    R = eye(3);
+end
+end
